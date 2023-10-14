47404822cba341f3c7d62ff9577b06999d23b2c7
ichaoX <11764590+ichaoX@users.noreply.github.com>
2023-10-15 00:16:27 +0800

    feat(external): check receiving end

--- src/lib/external.js
+++ src/lib/external.js
@@ -10,15 +10,29 @@ if (self.__fs_init) {
         console.debug(...args);
     };
 
+    let extMessage = typeof FS_EXT_MESSAGE !== "undefined" ? FS_EXT_MESSAGE : 'Using the "File System Access API" requires the "File System Access" extension (https://addons.mozilla.org/en-US/firefox/addon/file-system-access/) and helper app to be installed.';
+
     let sendMessage = async (action, data) => {
         try {
             action = `ext:${action}`;
             // console.log('request', action, data);
-            let response = await browser.runtime.sendMessage(extensionId, { action, data, origin: scope.origin || location.origin }, {});
+            let response;
+            try {
+                response = await browser.runtime.sendMessage(extensionId, { action, data, origin: scope.origin || location.origin }, {});
+            } catch (e) {
+                if (typeof extMessage === "string" && extMessage
+                    && /Receiving end does not exist/i.test((e && e.message) || "")
+                ) {
+                    let message = `${extMessage} \n\nError details: \n${e.message}`;
+                    throw new Error(message);
+                }
+                throw e;
+            }
             if (response.code !== 200) {
                 console.warn(response);
                 throw response.data || response;
             }
+            extMessage = false;
             // console.log('response', response);
             return response.data;
         } catch (e) {
